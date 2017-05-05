10fd8c30b6701c967ea7d91d987435e809fff9fe
waynebanks <thestorerun@yahoo.com>
2017-05-05 14:24:05 -0700

    heroku database configuration

--- database/db.js
+++ database/db.js
@@ -1,4 +1,7 @@
 const pgp = require('pg-promise')();
+if(process.env.NODE_ENV === 'production'){
+pgp.pg.defaults.ssl = true;
+};
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/waynes_world';
 const db = pgp(connectionString);
 
