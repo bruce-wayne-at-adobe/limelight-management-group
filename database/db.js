79dabfd1d3bbaa497fea61517f741b3817a5654d
waynebanks <thestorerun@yahoo.com>
2017-05-05 11:32:40 -0700

    db file adjustment for heroku

--- database/db.js
+++ database/db.js
@@ -2,6 +2,7 @@ const pgp = require('pg-promise')();
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/waynes_world';
 const db = pgp(connectionString);
 
+
 const queries = {
   getAll() {
     return db.any('SELECT * FROM blogs');
