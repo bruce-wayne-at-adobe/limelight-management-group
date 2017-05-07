106329c20dc7b57dcf7155075a2a8d6e8e9e294f
waynebanks <thestorerun@yahoo.com>
2017-05-06 17:26:09 -0700

    adjusted package.json and db file

--- database/db.js
+++ database/db.js
@@ -1,6 +1,5 @@
 const pgp = require('pg-promise')();
-
-const connectionString = 'postgres://localhost:5432/waynes_world';
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/waynes_world';
 const db = pgp(connectionString);
 
 const queries = {
