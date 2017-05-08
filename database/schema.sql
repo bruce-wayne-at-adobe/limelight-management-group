18520d4b4fe581708bf8eb0acf4af2ecf2e3ab23
waynebanks <thestorerun@yahoo.com>
2017-05-07 19:49:40 -0700

    schema file update

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,9 @@
--- DROP DATABASE IF EXISTS waynes_world;
+DROP DATABASE IF EXISTS waynes_world;
 CREATE DATABASE waynes_world; 
 
 \c waynes_world
 
+DROP TABLE IF EXISTS blogs;
 CREATE TABLE blogs (
 id SERIAL PRIMARY KEY,
 title VARCHAR(80) NOT NULL,
